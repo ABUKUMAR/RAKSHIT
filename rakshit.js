4058fb53de130e526090db3b859b0f387af7f320
ABUKUMAR <143511399+ABUKUMAR@users.noreply.github.com>
2023-08-29 18:06:37 +0530

    https://github.com/ABUKUMAR/RAKSHIT/upload/main

---
+++ rakshit.js
@@ -0,0 +1,22 @@
+
+function wellcome() { 
+  window.open("newtab.html")
+}
+
+function wellcomeone() { 
+    window.open("features.html")
+  }
+
+
+  function wellcometwo() { 
+    window.open("pricing.html")
+  }
+
+  function wellcomethree() { 
+    window.open("disable.html")
+  }
+
+
+  function wellcomehome() { 
+    window.open("rakshit.html")
+  }
